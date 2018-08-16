5e80a88c7002f1dd58c211b08c679d918ada5358
tanguband <tanguband@gmail.com>
2018-08-16 16:11:05 +0900

    バージョン表記　修正

--- src/init2.c
+++ src/init2.c
@@ -2489,9 +2489,9 @@ static void put_title(void)
 	char title[120];
 	int col;
 #if H_VER_EXTRA > 0
-	sprintf(title, _("√ª∂Ú»⁄≈‹ %d.%d.%d.%d(%s)", "Hengband %d.%d.%d.%d(%s)"), H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH, H_VER_EXTRA,
+	sprintf(title, _("√ª∂Ú»⁄≈‹ %d.%d.%d.%d(%s)", "tanguband %d.%d.%d.%d(%s)"), H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH, H_VER_EXTRA,
 #else
-	sprintf(title, _("√ª∂Ú»⁄≈‹ %d.%d.%d(%s)", "Hengband %d.%d.%d(%s)"), H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH,
+	sprintf(title, _("√ª∂Ú»⁄≈‹ %d.%d.%d(%s)", "tanguband %d.%d.%d(%s)"), H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH,
 #endif
 	IS_STABLE_VERSION ? _("∞¬ƒÍ»«", "Stable") : _("≥´»Ø»«", "Developing"));
 	col = (80 - strlen(title)) / 2;
